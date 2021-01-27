22184a6736b46806168148143a70e88637c78b32
Davi Silva <61832684+Davi-Code-Developer@users.noreply.github.com>
2021-01-27 19:59:33 -0300

    Criação do menu

---
+++ Projeto-Console/Projeto-Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Console
+{
+    class Program
+    {
+        enum Menu {Venda=1,Produto,Usuario,Cliente,Sair}
+        static void Main(string[] args)
+        {
+            bool sair = false;
+            while (sair == false)
+            {
+                Console.WriteLine("===Sistema de vendas Vidraçaria===");
+                Console.WriteLine("1-Venda\n2-Produto\n3-Usuario\n4-Cliente\n5-Sair");
+                Menu menu = (Menu)int.Parse(Console.ReadLine());
+                switch (menu)
+                {
+                    case Menu.Venda:
+                        break;
+                    case Menu.Produto:
+                        break;
+                    case Menu.Usuario:
+                        break;
+                    case Menu.Cliente:
+                        break;
+                    case Menu.Sair:
+                        sair = true;
+                        break;
+                }
+            }
+
+        }
+    }
+}

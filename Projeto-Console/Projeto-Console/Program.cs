0664448a9491465b7b5e46bc0dff594eb2193dd0
Davi Silva <61832684+Davi-Code-Developer@users.noreply.github.com>
2021-01-27 20:34:18 -0300

    Menu Part 2

--- Projeto-Console/Projeto-Console/Program.cs
+++ Projeto-Console/Projeto-Console/Program.cs
@@ -1,38 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Projeto_Console
 {
     class Program
     {
-        enum Menu {Venda=1,Produto,Usuario,Cliente,Sair}
+        enum Menu { Venda = 1, Estoque, Usuario, Cliente, Sair }
+        enum Menu_Produto { Visualizar = 1, Cadastrar, Atualizar, Excluir }
+        enum Menu_Usuario { Visualizar = 1, Cadastrar, Atualizar, Excluir }
+        enum Menu_Cliente { Visualizar = 1, Cadastrar, Atualizar, Excluir }
         static void Main(string[] args)
         {
             bool sair = false;
             while (sair == false)
             {
+                Console.Clear();
                 Console.WriteLine("===Sistema de vendas Vidraçaria===");
-                Console.WriteLine("1-Venda\n2-Produto\n3-Usuario\n4-Cliente\n5-Sair");
+                Console.WriteLine("1-Venda\n2-Estoque\n3-Usuario\n4-Cliente\n5-Sair");
                 Menu menu = (Menu)int.Parse(Console.ReadLine());
                 switch (menu)
                 {
                     case Menu.Venda:
                         break;
-                    case Menu.Produto:
+                    case Menu.Estoque:
+                        Console.Clear();
+                        Console.WriteLine("===Estoque===");
+                        Console.WriteLine("1-Visualizar\n2-Atualizar\n3-Adicionar\n4-Excluir");
+                        Menu_Produto menu_produto = (Menu_Produto)int.Parse(Console.ReadLine());
+                        switch (menu_produto)
+                        {
+                            case Menu_Produto.Visualizar:
+                                break;
+                            case Menu_Produto.Atualizar:
+                                break;
+                            case Menu_Produto.Cadastrar:
+                                break;
+                            case Menu_Produto.Excluir:
+                                break;
+                        }
                         break;
                     case Menu.Usuario:
+                        Console.Clear();
+                        Console.WriteLine("===Usuario===");
+                        Console.WriteLine("1-Visualizar\n2-Atualizar\n3-Adicionar\n4-Excluir");
+                        Menu_Usuario menu_usuario = (Menu_Usuario)int.Parse(Console.ReadLine());
+                        switch (menu_usuario)
+                        {
+                            case Menu_Usuario.Visualizar:
+                                break;
+                            case Menu_Usuario.Atualizar:
+                                break;
+                            case Menu_Usuario.Cadastrar:
+                                break;
+                            case Menu_Usuario.Excluir:
+                                break;
+                        }
                         break;
                     case Menu.Cliente:
+                        Console.Clear();
+                        Console.WriteLine("===Cliente===");
+                        Console.WriteLine("1-Visualizar\n2-Atualizar\n3-Adicionar\n4-Excluir");
+                        Menu_Cliente menu_cliente = (Menu_Cliente)int.Parse(Console.ReadLine());
+                        switch (menu_cliente)
+                        {
+                            case Menu_Cliente.Visualizar:
+                                break;
+                            case Menu_Cliente.Atualizar:
+                                break;
+                            case Menu_Cliente.Cadastrar:
+                                break;
+                            case Menu_Cliente.Excluir:
+                                break;
+                        }
                         break;
                     case Menu.Sair:
                         sair = true;
                         break;
                 }
             }
-
         }
     }
 }
